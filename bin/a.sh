6f9dc7704defc1be52be15743bf8e4bc3cc95f52
brajdic <32497664+brajdic@users.noreply.github.com>
2019-12-16 15:48:26 -0500

    Delete a.sh

--- bin/a.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sh D:/Desktop/temp/ushttp.sh;
-sh D:/Desktop/temp/ssl.sh;
-sh D:/Desktop/temp/nova.sh;
-sh D:/Desktop/temp/gmp.sh;
-sh D:/Desktop/temp/id.sh;
-cat D:/Desktop/temp/p.txt <(echo) D:/Desktop/temp/ssl.txt <(echo) D:/Desktop/temp/nova.txt <(echo) D:/Desktop/temp/g.txt <(echo) D:/Desktop/temp/id.txt | sed '/^$/d' > D:/Desktop/temp/all.txt
