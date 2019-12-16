716ca31d60fbbeac7f35c50d96c1357a80d46d57
brajdic <32497664+brajdic@users.noreply.github.com>
2019-12-16 15:56:30 -0500

    Add files via upload

--- src/com/GUI.java
+++ src/com/GUI.java
@@ -4,6 +4,8 @@ import javax.swing.JFrame;
 import javax.swing.UIManager;
 
 import java.awt.Color;
+import java.awt.Toolkit;
+
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -11,12 +13,15 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class GUI {
 
 	private JFrame frmProxyScraper;
 	private JComboBox<String> comboBox;
 	private JTextArea textAreaProxies;
+	private JLabel lblStatus;
 
 	/**
 	 * Launch the application.
@@ -46,38 +51,13 @@ public class GUI {
 		// JFrame
 		frmProxyScraper = new JFrame();
 		frmProxyScraper.getContentPane().setBackground(Color.DARK_GRAY);
-		frmProxyScraper.setTitle("Proxy Scraper v" + Proxy.VERSION);
+		frmProxyScraper.setTitle("Public Proxy Scraper");
 		frmProxyScraper.setBounds(100, 100, 360, 271);
 		frmProxyScraper.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmProxyScraper.getContentPane().setLayout(null);
-
-		// Scrape button
-		JButton btnScrape = new JButton("Scrape proxies");
-		btnScrape.setFocusable(false);
-		btnScrape.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				Proxy ps = new Proxy();
-				String proxies = ps.getProxies(comboBox.getSelectedIndex());
-				textAreaProxies.setText(proxies);
-			}
-		});
-		btnScrape.setBounds(40, 205, 120, 22);
-		frmProxyScraper.getContentPane().add(btnScrape);
-
-		// Combox
-		comboBox = new JComboBox<String>();
-		comboBox.setFocusable(false);
-		comboBox.setModel(
-				new DefaultComboBoxModel<String>(new String[] { "ALL", "USHTTP", "SSL", "NOVA", "GMP", "ID" }));
-		comboBox.setBounds(40, 28, 100, 22);
-		frmProxyScraper.getContentPane().add(comboBox);
-
-		// textAreaProxies
-		textAreaProxies = new JTextArea();
-		textAreaProxies.setBackground(Color.LIGHT_GRAY);
-		textAreaProxies.setBounds(40, 61, 272, 133);
-		frmProxyScraper.getContentPane().add(textAreaProxies);
-
+		frmProxyScraper.setIconImage(Toolkit.getDefaultToolkit().getImage(GUI.class.getResource("/com/images/ico.png")));
+		frmProxyScraper.setLocationRelativeTo(null);
+		
 		// clear button
 		JButton btnClear = new JButton("Clear");
 		btnClear.addActionListener(new ActionListener() {
@@ -89,8 +69,55 @@ public class GUI {
 		btnClear.setBounds(192, 205, 120, 22);
 		frmProxyScraper.getContentPane().add(btnClear);
 		
+		// Scrape button
+		JButton btnScrape = new JButton("Scrape proxies");
+		btnScrape.setFocusable(false);
+		btnScrape.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				Thread psThread = new Thread() {
+					@Override
+					public void run() {
+						lblStatus.setText("Establishing connection");
+						btnScrape.setEnabled(false);
+						btnClear.setEnabled(false);
+						Proxy ps = new Proxy();
+						String proxies = ps.getProxies(comboBox.getSelectedIndex());
+						textAreaProxies.setText(proxies);
+						lblStatus.setText("Finished with " + ps.getProxyCount() + " proxies");
+						btnScrape.setEnabled(true);
+						btnClear.setEnabled(true);
+					}
+				};
+				psThread.start();
+			}
+		});
+		btnScrape.setBounds(40, 205, 120, 22);
+		frmProxyScraper.getContentPane().add(btnScrape);
+
+		// Combox
+		comboBox = new JComboBox<String>();
+		comboBox.setFocusable(false);
+		comboBox.setModel(
+				new DefaultComboBoxModel<String>(new String[] {"ALL + remove duplicates", "USHTTP", "SSL", "NOVA", "GMP", "ID (takes some time)"}));
+		comboBox.setBounds(40, 28, 150, 22);
+		frmProxyScraper.getContentPane().add(comboBox);
+
+		// textAreaProxies
+		textAreaProxies = new JTextArea();
+		textAreaProxies.setBackground(Color.LIGHT_GRAY);
+		textAreaProxies.setBounds(40, 61, 272, 133);
+		frmProxyScraper.getContentPane().add(textAreaProxies);
+		
 		JScrollPane scrollPane = new JScrollPane(textAreaProxies);
 		scrollPane.setBounds(40, 61, 272, 133);
 		frmProxyScraper.getContentPane().add(scrollPane);
+		
+		lblStatus = new JLabel("");
+		lblStatus.setForeground(Color.WHITE);
+		lblStatus.setBackground(Color.WHITE);
+		lblStatus.setHorizontalAlignment(SwingConstants.CENTER);
+		lblStatus.setBounds(200, 28, 134, 20);
+		frmProxyScraper.getContentPane().add(lblStatus);
 	}
 }
--- src/com/Proxy.java
+++ src/com/Proxy.java
@@ -5,25 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.commons.lang3.StringUtils;
 
 public class Proxy {
-	
+
 	public static Double VERSION = 0.1;
 	private String strHtml;
-	private String strProxies;
 	private URLConnection spoof;
 	private URL url;
-	private String ip = "";
-	private String port = "";
-	
+	private String ipPattern = "^((0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)\\.){3}(0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)$";
+	private int proxyCount = 0;
+
 	public static void main(String[] args) {
 		GUI window = new GUI();
 		window.getFrame().setVisible(true);
 	}
 
 	public String getProxies(int index) {
-		switch(index) {
+		switch (index) {
 		case 0:
 			return getAll();
 		case 1:
@@ -41,99 +43,176 @@ public class Proxy {
 		}
 	}
 
+	private String removeDuplicates(String ids, String ushttp, String gmp, String ssl, String nova) {
+		String ips = ids + ushttp + gmp + ssl + nova;
+		proxyCount = 1;
+		String[] tokens = ips.split("\n");
+		StringBuilder resultBuilder = new StringBuilder();
+		Set<String> alreadyPresent = new HashSet<String>();
+		boolean first = true;
+		for (String token : tokens) {
+			if (!alreadyPresent.contains(token)) {
+				if (first)
+					first = false;
+				else {
+					resultBuilder.append("\n");
+					proxyCount++;
+				}
+				if (!alreadyPresent.contains(token))
+					resultBuilder.append(token);
+			}
+			alreadyPresent.add(token);
+		}
+		String removed = resultBuilder.toString();
+		return removed;
+	}
+
 	private String getID() {
-		return null;
+		String strProxiesID = "";
+		try {
+			url = new URL("http://www.idcloak.com/proxylist/free-us-proxy-list.html");
+			spoof = url.openConnection();
+			spoof.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0; H010818)");
+			BufferedReader in = new BufferedReader(new InputStreamReader(spoof.getInputStream()));
+			String[] tds = getSource(in, url, "");
+			for (int i = 0; i < tds.length - 1; i++) {
+				if (tds != null)
+					if (tds[i + 1].matches(ipPattern)) { // check if next element in array is an ip address
+						strProxiesID += tds[i + 1] + ":";
+						strProxiesID += tds[i] + "\n";
+						proxyCount++;
+					}
+			}
+		} catch (IOException e) {
+		}
+		return strProxiesID;
 	}
 
 	private String getNova() {
-		return null;
+		String strProxiesNOVA = "";
+		try {
+			url = new URL("https://www.proxynova.com/proxy-server-list/port-8080/");
+			spoof = url.openConnection();
+			spoof.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0; H010818)");
+			BufferedReader in = new BufferedReader(new InputStreamReader(spoof.getInputStream()));
+			String[] ips = getSource(in, url, "ip");
+			String[] ports = getSource(in, url, "port");
+
+			if (ips != null && ports != null)
+				for (int i = 0; i < ips.length; i++) {
+					if (ips[i].matches(ipPattern)) {
+						strProxiesNOVA += ips[i] + ":" + ports[i] + "\n";
+						proxyCount++;
+					}
+				}
+		} catch (IOException e) {
+		}
+		return strProxiesNOVA;
 	}
 
 	private String getGMP() {
+		String strProxiesGMP = "";
 		try {
 			url = new URL("http://givemeproxy.com/");
 			spoof = url.openConnection();
 			spoof.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0; H010818)");
 			BufferedReader in = new BufferedReader(new InputStreamReader(spoof.getInputStream()));
 			String[] ips = getSource(in, url, "ip");
-			strProxies = "";
-			ip = "";
-			if(ips != null)
-				for(int i = 0; i < ips.length; i++) { //gets the ips
-						ip += ips[i] + "\n";
-			}
 			String[] ports = getSource(in, url, "port");
-			if(ports != null)
-				for(int i = 0; i < ports.length; i++) { //gets the ports
-						port += ports[i] + "\n";
-			}
+
+			if (ips != null && ports != null)
+				for (int i = 0; i < ips.length; i++) {
+					strProxiesGMP += ips[i] + ":" + ports[i] + "\n";
+					proxyCount++;
+				}
 		} catch (IOException e) {
 		}
-		return strProxies + ip + port; //remove null first line and combine both first lines of strings
+		return strProxiesGMP;
 	}
 
 	private String getAll() {
-		String all = "";
+		String ids = getID();
 		String ushttp = getUSHTTP();
+		String gmp = getGMP();
 		String ssl = getSSL();
-		all = ushttp + ssl;
+		String nova = getNova();
+		String all = removeDuplicates(ids, ushttp, gmp, ssl, nova);
 		return all;
 	}
 
-	private String[] getSource(BufferedReader in, URL url, String pattern) throws IOException {
+	private String[] getSource(BufferedReader in, URL url, String ipPort) throws IOException {
+		String strProxies = "";
 		String[] tds;
 		while ((strProxies = in.readLine()) != null) {
 			strHtml += strProxies;
 		}
-		URL test = new URL("http://givemeproxy.com/");
-		if(url.sameFile(test) && pattern == "ip") 
+		if (url.toString().equals("http://givemeproxy.com/") && ipPort.equals("ip"))
 			tds = StringUtils.substringsBetween(strHtml, "<td class=\"column-1\">", "</td>");
-		else if(url.sameFile(test) && pattern == "port") 
+		else if (url.toString().equals("http://givemeproxy.com/") && ipPort.equals("port"))
 			tds = StringUtils.substringsBetween(strHtml, "<td class=\"column-2\">", "</td>");
+		else if (url.toString().equals("https://www.proxynova.com/proxy-server-list/port-8080/") && ipPort.equals("ip"))
+			tds = StringUtils.substringsBetween(strHtml, "<abbr title=\"", "\">");
+		else if (url.toString().equals("https://www.proxynova.com/proxy-server-list/port-8080/")
+				&& ipPort.equals("port"))
+			tds = StringUtils.substringsBetween(strHtml,
+					"<a href=\"/proxy-server-list/port-8080/\" title=\"Port 8080 proxies\">", "</a>");
 		else
 			tds = StringUtils.substringsBetween(strHtml, "<td>", "</td>");
 		return tds;
 	}
-	
-	public String getUSHTTP() {
+
+	private String getUSHTTP() {
+		String strProxiesHTTP = "";
 		try {
 			url = new URL("https://www.us-proxy.org/");
 			spoof = url.openConnection();
 			spoof.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0; H010818)");
 			BufferedReader in = new BufferedReader(new InputStreamReader(spoof.getInputStream()));
 			String[] tds = getSource(in, url, "");
-			strProxies = "";
-			if(tds != null)
-				for(int i = 0; i < tds.length; i++) {
-					if(!tds[i].contains("-") && !tds[i].contains(":")) //try and get only IP and port
-						if(tds[i].contains(".")) 
-							if(tds[i + 1].matches("[0-9]{1,5}$")) //make sure the next element is a port
-								strProxies += tds[i] + ":" + tds[i + 1] + "\n";
-			}
+			strProxiesHTTP = "";
+			if (tds != null)
+				for (int i = 0; i < tds.length; i++) {
+					if (!tds[i].contains("-") && !tds[i].contains(":")) // try and get only IP and port
+						if (tds[i].contains("."))
+							if (tds[i + 1].matches("[0-9]{1,5}$"))  { // make sure the next element is a port
+								strProxiesHTTP += tds[i] + ":" + tds[i + 1] + "\n";
+								proxyCount++;
+							}
+				}
 		} catch (IOException e) {
 		}
-		return strProxies;
+		return strProxiesHTTP;
 	}
 
-	
-	public String getSSL() {
+	private String getSSL() {
+		String strProxiesSSL = "";
 		try {
 			url = new URL("https://www.sslproxies.org/");
 			spoof = url.openConnection();
 			spoof.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0; H010818)");
 			BufferedReader in = new BufferedReader(new InputStreamReader(spoof.getInputStream()));
 			String[] tds = getSource(in, url, "");
-			strProxies = "";
-			if(tds != null)
-				for(int i = 0; i < tds.length; i++) {
-					if(!tds[i].contains("-") && !tds[i].contains(":")) //try and get only IP and port
-						if(tds[i].contains(".")) 
-							if(tds[i + 1].matches("[0-9]{1,5}$")) //make sure the next element is a port
-									strProxies += tds[i] + ":" + tds[i + 1] + "\n";
-			}
+			strProxiesSSL = "";
+			if (tds != null)
+				for (int i = 0; i < tds.length; i++) {
+					if (!tds[i].contains("-") && !tds[i].contains(":")) // try and get only IP and port
+						if (tds[i].contains("."))
+							if (tds[i + 1].matches("[0-9]{1,5}$")) { // make sure the next element is a port
+								strProxiesSSL += tds[i] + ":" + tds[i + 1] + "\n"; 
+								proxyCount++;
+							}
+				}
 		} catch (IOException e) {
 		}
-		return strProxies;
+		return strProxiesSSL;
 	}
-	
+
+	public int getProxyCount() {
+		return proxyCount;
+	}
+
+	public void setProxyCount(int proxyCount) {
+		this.proxyCount = proxyCount;
+	}
+
 }
